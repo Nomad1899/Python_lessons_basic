f6fd48c846654c21a3f7a81ea8214f06eb63fc0a
Nomad1899 <45853785+Nomad1899@users.noreply.github.com>
2019-01-22 20:52:04 +0300

    Выполнил easy

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -5,14 +5,17 @@
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
-a=[1, 2, 4, 0]
-b=[i**2 for i in a]
+a = [1, 2, 4, 0]
+b = [i ** 2 for i in a]
 print(b)
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
-
+bob_fruits = ['orange', 'Blueberries', 'banana', 'Grapefruit', 'Lemon', 'Mandarin', 'Papaya']
+jonh_fruits = ['Peaches', 'Lemon', 'Raspberries', 'Blueberries', 'Mandarin', 'Watermelon']
+fruits = [x for x in bob_fruits if x in jonh_fruits]
+print (fruits)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -20,3 +23,8 @@ print(b)
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+import random
+numbers=[random.randint(0,100) for m in range(10)]
+print (numbers)
+my_numbers=[x for x in numbers if x%3==0 and x>0 and x%4!=0]
+print (my_numbers)
