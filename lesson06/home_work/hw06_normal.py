773df2aceb76dbc07fc412eb40e8768425914905
Sergey Smirnov <nomad1899@gmail.com>
2019-01-31 09:46:47 +0300

    начал normal

--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,30 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class Person:
+    def __init__(self,last_name, first_name, second_name):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.second_name = second_name
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.second_name}'
+
+class Parent(Person):
+    def __init__(self, last_name, first_name, second_name):
+        super().__init__(last_name, first_name, second_name)
+        self.children = []
+
+
+class Student(Person):
+    def __init__(self, last_name, first_name, second_name, mother: Parent, father: Parent):
+        super().__init__(last_name, first_name, second_name)
+        self.class_group = None
+        self.mother = mother
+        self.father = father
+        mother.children.append(self)
+        father.children.append(self)
+
+    def set_class_group(self, class_group):
+        self.class_group = class_group
