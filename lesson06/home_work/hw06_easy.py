5a41832a235b1202bb47a815ac525e3da8f3b2d2
Sergey Smirnov <nomad1899@gmail.com>
2019-02-04 08:21:11 +0300

    переделал дз 06

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -2,36 +2,74 @@
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 from math import sqrt
 
+class Vector:
 
-class Triangle(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __str__(self):
+        return f'( {self.start}; {self.end} )'
+
+    def __get_legs(self):
+        k1 = abs(self.start[0] - self.end[0])
+        k2 = abs(self.start[1] - self.end[1])
+        return k1, k2
+
+    def get_length(self):
+        k1, k2 = self.__get_legs()
+        return (k1 ** 2 + k2 ** 2) ** (1 / 2)
+
+    def get_point(self):
+        return self.end[0] - self.start[0], self.end[1] - self.start[1]
+
+    def get_matrix(self):
+        return [[self.start[0], self.end[0]], [self.start[1], self.end[1]]]
+
+
+class Shape:
+
+    def __init__(self, points):
+        """
+        Base class shapes
+        :param points: List of tuples with coord X, Y
+        """
+        count = len(points)
+        self.vectors = []
+        for i in range(count):
+            self.vectors.append(Vector(points[i], points[((i + 1) % count)]))
+
+    def get_square(self):
+        vectors_det = []
+        for v in self.vectors:
+            m = v.get_matrix()
+            vectors_det.append(m[0][0] * m[1][1] - m[0][1] * m[1][0])
+        return abs(sum(vectors_det) / 2)
+
+    def get_perimeter(self):
+        return sum([v.get_length() for v in self.vectors]
+
+
+class Triangle(Shape):
+
     def __init__(self, a, b, c):
-        self._a = a
-        self._b = b
-        self._c = c
+        """
+        Triangle class
+        :param a: Tuple with coord X, Y
+        :param b: Tuple with coord X, Y
+        :param c: Tuple with coord X, Y
+        """
+        super().__init__([a, b, c])
 
-    @property
-    def square(self):
-        return 0.5 * abs((self._b[0] - self._a[0]) * (self._c[1] - self._a[1]) - (self._c[0] - self._a[0]) * (
-                self._b[1] - self._a[1]))
+    def get_triangle_square(self):
+        pp = self.get_perimeter() / 2
+        a = self.vectors[0].get_length()
+        b = self.vectors[1].get_length()
+        c = self.vectors[2].get_length()
+        return (pp * (pp - a) * (pp - b) * (pp - c)) ** (1/2)
 
-    def long(self, x, y):
-        return sqrt((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2)
-
-    @property
-    def perimeter(self):
-        return self.long(self._a, self._b) + self.long(self._b, self._c) + self.long(self._a, self._c)
-
-    @property
-    def height(self):
-        return [2 * self.square / self.long(self._a, self._b), 2 * self.square / self.long(self._a, self._c),
-                2 * self.square / self.long(self._b, self._c)]
-
-
-trin = Triangle([2, 10], [5, 4], [8, 3])
-print(trin.square)
-print (trin.long(trin._a, trin._b))
-print (trin.perimeter)
-print (trin.height)
+    def get_height(self, vector_num=0):
+        return self.get_square() * 2 / self.vectors[vector_num].get_length()
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -39,6 +77,32 @@ print (trin.height)
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
-class Isosceles_trapezium:
+class Trapeze(Shape):
+
     def __init__(self, a, b, c, d):
-        
+        """
+        Trapeze class
+        :param a: Tuple with coord X, Y
+        :param b: Tuple with coord X, Y
+        :param c: Tuple with coord X, Y
+        :param d: Tuple with coord X, Y
+        """
+        super().__init__([a, b, c, d])
+
+    def check_equilateral(self):
+
+        def __check_equal_diagonal(vector1, vector2):
+            d1 = Vector(vector1.start, vector2.start)
+            d2 = Vector(vector1.end, vector2.end)
+            return d1.get_length() == d2.get_length()
+
+        if self.vectors[0].get_length() == self.vectors[2].get_length():
+            return __check_equal_diagonal(self.vectors[0], self.vectors[2])
+        elif self.vectors[1].get_length() == self.vectors[3].get_length():
+            return __check_equal_diagonal(self.vectors[1], self.vectors[3])
+        else:
+            return False
+
+    def get_length_of_sides(self):
+        for i in range(len(self.vectors)):
+            yield self.vectors[i].get_length()
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -16,14 +16,21 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
+import random
+import string
+import os
+from typing import List
+
+
 class Person:
-    def __init__(self,last_name, first_name, second_name):
+    def __init__(self, last_name, first_name, second_name):
         self.last_name = last_name
         self.first_name = first_name
         self.second_name = second_name
 
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.second_name}'
+
 
 class Parent(Person):
     def __init__(self, last_name, first_name, second_name):
@@ -42,3 +49,191 @@ class Student(Person):
 
     def set_class_group(self, class_group):
         self.class_group = class_group
+
+
+class Teacher(Person):
+    def __init__(self, last_name, first_name, second_name, lesson_subject):
+        super().__init__(last_name, first_name, second_name)
+        self.lesson_subject = lesson_subject
+        self.class_groups = []
+
+    def __str__(self):
+        return f'{super().__str__()} ({self.lesson_subject})'
+
+
+class ClassGroup:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        self.lesson_teacher = {}
+
+    def add_students(self, studs):
+        for s in studs:
+            self.add_student(s)
+
+    def add_student(self, stud):
+        stud.set_class_group(self)
+        self.students.append(stud)
+
+    def set_lesson_teacher(self, teacher: Teacher):
+        self.lesson_teacher[teacher.lesson_subject] = teacher
+        teacher.class_groups.append(self)
+
+    def get_all_student_names(self):
+        return [st.__str__() for st in self.students]
+
+    def __str__(self):
+        return f'{self.name} Students count: {len(self.students)}'
+
+
+class School:
+    def __init__(self):
+        self.class_groups: List[ClassGroup] = []
+        self.all_studs: List[Student] = []
+
+    def get_all_classes_names(self):
+        return [gr.name for gr in self.class_groups]
+
+    def get_all_students(self):
+        if len(self.all_studs) == 0:
+            for gr in self.class_groups:
+                for st in gr.students:
+                    self.all_studs.append(st)
+        return self.all_studs
+
+
+# ======================================================= #
+
+
+lessons = ['Алгебра', 'Геометрия', 'Русский язык', 'Литература', 'География', 'Биология']
+names = {
+    'f_last_names': [],
+    'm_last_names': [],
+    'f_first_names': [],
+    'm_first_names': [],
+    'f_second_names': [],
+    'm_second_names': [],
+}
+
+
+def load_names():
+    global names
+    for name, collection in names.items():
+        file = f'.\\names\\{name}.txt'
+        if os.path.isfile(file):
+            with open(file, 'r', encoding='utf-8') as file:
+                names[name] = [line.strip() for line in file.readlines()]
+
+
+def get_random_item(collection):
+    return collection[random.randrange(len(collection))]
+
+
+def get_person(is_male):
+    if is_male:
+        ln = get_random_item(names['m_last_names'])
+        fn = get_random_item(names['m_first_names'])
+        sn = get_random_item(names['m_second_names'])
+    else:
+        ln = get_random_item(names['f_last_names'])
+        fn = get_random_item(names['f_first_names'])
+        sn = get_random_item(names['f_second_names'])
+    return ln, fn, sn
+
+
+def gen_studs(count):
+    result = []
+    for i in range(count):
+        m_last_name = get_random_item(names['m_last_names'])
+        f_last_name = [x for x in names['f_last_names'] if str(x).startswith(m_last_name[:-3])]
+        if len(f_last_name) == 0:
+            f_last_name = get_random_item(names['f_last_names'])
+        else:
+            f_last_name = f_last_name[0]
+
+        ln, fn, sn = get_person(False)
+        mother = Parent(f_last_name, fn, sn)
+        ln, fn, sn = get_person(True)
+        father = Parent(m_last_name, fn, sn)
+
+        s = random.choice([True, False])
+        ln, fn, sn = get_person(s)
+        if s:
+            ln = m_last_name
+        else:
+            ln = f_last_name
+
+        # TODO fix student second name
+
+        result.append(Student(ln, fn, sn, mother, father))
+    return result
+
+
+def gen_teachers(count):
+    result = []
+    for i in range(count):
+        ln, fn, sn = get_person(random.choice([True, False]))
+        result.append(Teacher(ln, fn, sn, get_random_item(lessons)))
+    return result
+
+
+def gen_class_groups(count):
+    result = []
+    teachers = gen_teachers(len(lessons)*2)
+    for i in range(count):
+        while True:
+            group = ClassGroup(f'{random.choice(string.digits[1:])}{random.choice("АБВГД")}')
+            if group.name not in [r.name for r in result]:
+                break
+
+        group.add_students(gen_studs(10))
+        for i in range(len(lessons)):
+            k = 0
+            while True:
+                t = random.choice(teachers)
+                if t.lesson_subject not in group.lesson_teacher.keys() or k == 10:
+                    break
+                k += 1
+
+            if k >= 10:
+                continue
+            group.set_lesson_teacher(t)
+        result.append(group)
+    return result
+
+
+def main():
+    load_names()
+    school = School()
+    school.class_groups = gen_class_groups(10)
+    print('1. Получить полный список всех классов школы')
+    print('Полный список всех классов школы')
+    print(','.join(sorted(school.get_all_classes_names())))
+    print('-'*50)
+
+    print('2. Получить список всех учеников в указанном классе')
+    gr = get_random_item(school.class_groups)
+    print(f'Случайный класс - {gr.name}:')
+    print('\n'.join(sorted(gr.get_all_student_names())))
+    print('-' * 50)
+
+    print('3. Получить список всех предметов указанного ученика')
+    st = get_random_item(school.get_all_students())
+    print(f'Случайный ученик - {st.__str__()}:')
+    print('\n'.join(list(st.class_group.lesson_teacher.keys())))
+    print('-' * 50)
+
+    print('4. Узнать ФИО родителей указанного ученика')
+    st = get_random_item(school.all_studs)
+    print(f'Случайный ученик - {st.__str__()}:')
+    print(f'Mother - {st.mother}\nFather - {st.father}')
+    print('-' * 50)
+
+    print('5. Получить список всех Учителей, преподающих в указанном классе')
+    gr = get_random_item(school.class_groups)
+    print(f'Случайный класс - {gr.name}:')
+    print('\n'.join([t.__str__() for t in gr.lesson_teacher.values()]))
+
+
+if __name__ == '__main__':
+    main()
